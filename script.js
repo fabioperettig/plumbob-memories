baf7b30f7934455a5cd51ff1ed7c5f8657a55b30
Fabio Peretti Guimarães <fabioperetti_pp@outlook.com>
2024-01-24 19:22:12 -0300

    up

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.querySelector('.btn1').addEventListener('click', () => {
+    
+    document.querySelector('.modWhats').scrollIntoView({ behavior: 'smooth' });
+    
+})
+
+document.querySelector('.btn5').addEventListener('click', () => {
+    
+    document.querySelector('.modWhats').scrollIntoView({ behavior: 'smooth' });
+    
+})
